480911bbc6eb1c1f0da675e4c1ad2d87d51f1f4a
nakatani katsumi <alchu6636@gmail.com>
2012-08-16 21:19:52 +0900

    setup variables for test

--- test_zn.py
+++ test_zn.py
@@ -11,6 +11,12 @@ class TestZn(unittest.TestCase):
         self.z10 = Zn(10)
         self.z99 = Zn(99)
         self.z100 = Zn(100)
+        self.t2 = Zn.TableFormat(2)
+        self.t3 = Zn.TableFormat(3)
+        self.t9 = Zn.TableFormat(9)
+        self.t10 = Zn.TableFormat(10)
+        self.t99 = Zn.TableFormat(99)
+        self.t100 = Zn.TableFormat(100)
 
     def stub_test_str(self):
         result = self.z2.__str__()
@@ -25,31 +31,31 @@ class TestZn(unittest.TestCase):
         self.assertRaises(ValueError, Zn, 1)
         
     def test_col_width(self):
-        self.assertEqual(Zn.TableFormat(2)._col_width(), 2)
-        self.assertEqual(Zn.TableFormat(9)._col_width(), 2)
-        self.assertEqual(Zn.TableFormat(10)._col_width(), 3)
-        self.assertEqual(Zn.TableFormat(99)._col_width(), 3)
-        self.assertEqual(Zn.TableFormat(100)._col_width(), 4)
+        self.assertEqual(self.t2._col_width(), 2)
+        self.assertEqual(self.t3._col_width(), 2)
+        self.assertEqual(self.t10._col_width(), 3)
+        self.assertEqual(self.t99._col_width(), 3)
+        self.assertEqual(self.t100._col_width(), 4)
 
     def test_cap_data(self):
-        self.assertEqual(Zn.Cap(Zn.TableFormat(2)).data(), " 0/ 1".split("/"))
-        self.assertEqual(Zn.Cap(Zn.TableFormat(3)).data(), " 0/ 1/ 2".split("/"))
+        self.assertEqual(Zn.Cap(self.t2).data(), " 0/ 1".split("/"))
+        self.assertEqual(Zn.Cap(self.t3).data(), " 0/ 1/ 2".split("/"))
 
     def test_data_data(self):
-        self.assertEqual(Zn.Data(1, Zn.TableFormat(2)).data(), " 0/ 1".split("/"))
-        self.assertEqual(Zn.Data(2, Zn.TableFormat(3)).data(), " 0/ 2/ 1".split("/"))
-
+        self.assertEqual(Zn.Data(1, self.t2).data(), " 0/ 1".split("/"))
+        self.assertEqual(Zn.Data(2, self.t3).data(), " 0/ 2/ 1".split("/"))
+        
     def test_cap(self):
-        self.assertEqual(Zn.StringTable(Zn.TableFormat(2)).cap(), " *| 0 1")
-        self.assertEqual(Zn.StringTable(Zn.TableFormat(10)).cap(), "  *|  0  1  2  3  4  5  6  7  8  9")
+        self.assertEqual(Zn.StringTable(self.t2).cap(), " *| 0 1")
+        self.assertEqual(Zn.StringTable(self.t10).cap(), "  *|  0  1  2  3  4  5  6  7  8  9")
 
     def test_data(self):
-        self.assertEqual(Zn.Data(1, Zn.TableFormat(2)).output(), " 1/|/ 0/ 1".split("/"))
-        self.assertEqual(Zn.Data(2, Zn.TableFormat(3)).output(), " 2/|/ 0/ 2/ 1".split("/"))
+        self.assertEqual(Zn.Data(1, self.t2).output(), " 1/|/ 0/ 1".split("/"))
+        self.assertEqual(Zn.Data(2, self.t3).output(), " 2/|/ 0/ 2/ 1".split("/"))
 
     def test_sep(self):
-        self.assertEqual(Zn.StringTable(Zn.TableFormat(2)).sep(), "--+----")
-        self.assertEqual(Zn.StringTable(Zn.TableFormat(3)).sep(), "--+------")
+        self.assertEqual(Zn.StringTable(self.t2).sep(), "--+----")
+        self.assertEqual(Zn.StringTable(self.t3).sep(), "--+------")
 
     def test_str_multi2(self):
         self.assertEqual(self.z2.str_multi_table(), \
